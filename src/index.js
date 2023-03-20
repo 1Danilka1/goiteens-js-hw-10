f387eae4460fb1b65b7b1ecbf6632c4419cde335
Danyl Brahan <danylbragan@gmail.com>
2023-03-20 17:55:36 +0100

    d

---
+++ src/index.js
@@ -0,0 +1,53 @@
+// ============================================================ №1 ==============================================================================
+let counterValue = 0;
+const counterSpan = document.querySelector('#value');
+const positiveButton = document.querySelector(`[data-action="increment"]`);
+const negativeButton = document.querySelector(`[data-action="decrement"]`);
+
+
+
+const onNegativeClick = () => {
+  counterValue -= 1;
+  counterSpan.textContent = counterValue;
+}
+
+const onPositiveClick = () => {
+  counterValue += 1;
+  counterSpan.textContent = counterValue;
+}
+
+negativeButton.addEventListener('click', onNegativeClick);
+
+positiveButton.addEventListener('click', onPositiveClick);
+// ============================================================ №2 ==============================================================================
+// const textInput = document.querySelector('#name-input');
+// const span = document.querySelector('#name-output');
+
+// const getInputValue = ({ currentTarget }) => {
+//   if (currentTarget.value.trim() !== '') {
+//     span.textContent = currentTarget.value.trim();
+//   } else {
+//     span.textContent = 'Anonymous'
+//   }
+// };
+
+// textInput.addEventListener('input', getInputValue)
+
+
+
+// const button = document.querySelector(".change-color");
+// const span = document.querySelector(".color");
+// const body = document.body;
+
+// const onChangeColour = () => {
+//     span.textContent = body.style.backgroundcolor = getRandomHexColor();
+// }
+
+// button.addEventListener("click", onChangeColour)
+
+
+// function getRandomHexColor() {
+//     return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
