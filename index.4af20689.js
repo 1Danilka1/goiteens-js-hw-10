c650ddfd74ea98af0b0699530a815dbc835df7e8
1Danilka1 <110053830+1Danilka1@users.noreply.github.com>
2023-03-21 16:05:20 +0000

    Deploying to gh-pages from @ 1Danilka1/goiteens-js-hw-10@0b3caed1af4332a2d7706375b567962a6105205f 🚀

--- index.4af20689.js
+++
@@ -1,2 +0,0 @@
-let e=0;const t=document.querySelector("#value"),n=document.querySelector('[data-action="increment"]');document.querySelector('[data-action="decrement"]').addEventListener("click",(()=>{e-=1,t.textContent=e})),n.addEventListener("click",(()=>{e+=1,t.textContent=e}));
-//# sourceMappingURL=index.4af20689.js.map
